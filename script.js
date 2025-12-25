b1cbfaff7be054e07fe6da7e6d3f5d672b7034c7
My Sidhi <sidhimy@gmail.com>
2025-12-26 00:28:34 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 setTimeout((5) => {
   document.getElementById("popup").style.display = "flex";
 }, 5000);
@@ -5,3 +6,18 @@ setTimeout((5) => {
 function closePopup() {
   document.getElementById("popup").style.display = "none";
 }
+
+// fade animation when scrolling
+const items = document.querySelectorAll("[data-anim]");
+
+function animate() {
+  items.forEach(el=>{
+    const rect = el.getBoundingClientRect().top;
+    if(rect < window.innerHeight - 80){
+      el.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", animate);
+animate(); // run once on load
